67e396ee328880359771fde4d59ce0c2b4b5672e
unknown <danipaspastor@gmail.com>
2018-05-03 16:03:55 +0200

    first commit

---
+++ src/personas/Persona.java
@@ -0,0 +1,86 @@
+package personas;
+
+public class Persona {
+
+	private String colorOjos;
+	private String colorPelo;
+	private String tipoPelo;
+	private String tipoOjos;
+	private String nombre;
+	private String ciudad;
+	private int edad;
+	private int peso;
+	private int altura;
+
+	public String getColorOjos() {
+		return this.colorOjos;
+	}
+
+	public void setColorOjos(String _colorOjos) {
+		this.colorOjos = _colorOjos;
+	}
+
+	public String getColorPelo() {
+		return this.colorPelo;
+	}
+
+	public void setColorPelo(String _colorPelo) {
+		this.colorPelo = _colorPelo;
+	}
+
+	public String getTipoPelo() {
+		return this.tipoPelo;
+	}
+
+	public void setTipoPelo(String _tipoPelo) {
+		this.tipoPelo = _tipoPelo;
+	}
+
+	public String getTipoOjos() {
+		return this.tipoOjos;
+	}
+
+	public void setTipoOjos(String _tipoOjos) {
+		this.tipoOjos = _tipoOjos;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String _nombre) {
+		this.nombre = _nombre;
+	}
+
+	public String getCiudad() {
+		return this.ciudad;
+	}
+
+	public void setCiudad(String _ciudad) {
+		this.ciudad = _ciudad;
+	}
+
+	public int getEdad() {
+		return this.edad;
+	}
+
+	public void setEdad(int _edad) {
+		this.edad = _edad;
+	}
+
+	public int getPeso() {
+		return this.peso;
+	}
+
+	public void setPeso(int _peso) {
+		this.peso = _peso;
+	}
+
+	public int getAltura() {
+		return this.altura;
+	}
+
+	public void setAltura(int _altura) {
+		this.altura = _altura;
+	}
+}
